d96824575e917fcabaa2ca6ff6bed3a8858e166d
Guizani, Mariam <mariam.guizani@gmail.com>
2017-09-29 14:04:52 -0700

    timer notes and todos

--- account/views.py
+++ account/views.py
@@ -11,3 +11,12 @@ def home(request):
 def login(request):
 
     return render(request, 'account/login.html')
+
+def startTimer(request):
+    # TODO named tuple?
+    self.new_interval = Interval(course=course_id, start_time=time.time())  # some data structure for interval
+
+def endTimer(request):
+    self.new_interval.end_time = time.time()
+    # TODO send to sql
+    self.new_interval = None

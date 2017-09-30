efbbcfc6f420c12b21f3a5d8569c0c07190adf3c
Alex <turneral1@grinnell.edu>
2017-09-29 17:41:14 -0700

    Implemented skeleton code for course creation and timer management.

--- account/views.py
+++ account/views.py
@@ -1,22 +1,36 @@
+import time
+from collections import namedtuple
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 # function based view
 def home(request):
-
-    name = "Traker"
+    name = "Tracker"
     args = {'name': name}
     return render(request, 'account/home.html', args)
 
 def login(request):
-
     return render(request, 'account/login.html')
 
+def createCourse(request):
+    """Ask the user for the name of the course they want to create."""
+    course_name = UI.getCourseName()  # TODO implement
+    if course_name not in self.courses:
+        self.courses.add(course_name)  # TODO initialize this set somewhere
+    else:
+        UI.display("That course name already exists. Try again?")
+        if UI.tryAgain(): createCourse(request)
+
 def startTimer(request):
-    # TODO named tuple?
-    self.new_interval = Interval(course=course_id, start_time=time.time())  # some data structure for interval
+    """Ask the user what course they're working on and then start the timer."""
+    course_name = UI.getCourseChoice()  # TODO implement
+    self.new_interval = namedtuple(course=course_name, start_time=time.time(), end_time=None)  # TODO store appropriately
 
 def endTimer(request):
+    """End the current timer.
+
+    Assumes that startTimer() has been called
+    """
     self.new_interval.end_time = time.time()
     # TODO send to sql
     self.new_interval = None

16a9a724eef05d30cde10c80ce0ba9dfc0a5a0d2
Alex <turneral1@grinnell.edu>
2017-10-12 10:26:47 -0700

    Fixed an issue where invalid signup forms weren't being handled. Removed redundant home code. Fixed an issue with including an extra dict in redirect parameters. Required login for accessing the courses module. Merged course display and addition pages. Made a rudimentary table for course display (working on integrating with context). Restricted sign-up to users who are not already logged in.

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.views.generic import *
 from django.shortcuts import render
 from courses.forms import CourseForm
 from courses.models import Course
 
 
+class CourseView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(CourseView, self).get_context_data(**kwargs)
+        context['courses'] = Course.objects.order_by('name')  # TODO embed so page can display currently-created classes
+        return context
+
 @login_required
 def index(request):
     """List the entered courses and ask the user for the name of the course they want to create."""
-    #context['courses'] = Course.objects.order_by('name')  # TODO embed so page can display currently-created classes
     if request.method == 'POST':
         name_form = CourseForm(request.POST)
         if name_form.is_valid():

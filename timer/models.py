441db667d9a0a07221f32d8ead7c8e490003bf99
Alex Turner <turneral1@grinnell.edu>
2017-10-12 15:43:23 -0700

    Started implementing Timer.

--- timer/models.py
+++ timer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from courses import Course
 
-# Create your models here.
+
+class TimeInterval(models.Model):
+    """Create a time interval showing when the user worked on a course."""
+    course = Course()  # the course being worked on
+    start_time = models.DateTimeField(auto_now_add=True)  # defaults to creation time
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return "{}: {} to {}".format(self.course, self.start_time, self.end_time)
--- timer/views.py
+++ timer/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
-import time
-from collections import namedtuple
+import timer.models
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
 
-def startTimer(request):
+def start(request):
     """Ask the user what course they're working on and then start the timer."""
     course_name = UI.getCourseChoice()  # TODO implement
+    self.new_interval = TimeInterval()
     self.new_interval = namedtuple(course=course_name, start_time=time.time(), end_time=None)  # TODO store appropriately
 
-def endTimer(request):
+
+def end(request):
     """End the current timer.
 
-    Assumes that startTimer() has been called
+    Assumes that start() has been called
     """
-    self.new_interval.end_time = time.time()
-    # TODO send to sql database
-    self.new_interval = None  # we're done with the most recently-recorded interval, so clear
+    if request.method == 'POST':
+        #new_interval =
+        new_interval.end_time = datetime.today()
+        new_interval.save(commit=True)
+        return render(request, 'courses')  # TODO go to courses screen?
+
+    return render(request, 'timer')

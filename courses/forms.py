c1838451f1a63e7dc5c4e31add5e7993281fabbe
Alex Turner <turneral1@grinnell.edu>
2017-10-15 19:44:59 -0700

    Removed unnecessary CourseForm help text. Simplified HTML for Course addition.

--- courses/forms.py
+++ courses/forms.py
@@ -5,9 +5,6 @@ from django.contrib.auth.models import User
 
 class CourseForm(forms.ModelForm):
     """Creates a Form from the Course model."""
-    name = forms.CharField(max_length=50, help_text='Please enter the course name.')
-    hours = forms.IntegerField(min_value=1, help_text='Please enter how many hours you want to work on this each week.')
-
     def is_valid(self, user):
         """Checks if the user has already created an identically-named course, in addition to super().is_valid()."""
         if not super().is_valid():  # see if the form is otherwise valid

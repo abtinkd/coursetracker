b52e636bafa6ad4764df4575391cca9ae8e8a398
Guizani, Mariam <mariam.guizani@gmail.com>
2017-11-18 12:22:24 -0800

    Revert "postgres database password removed from setting.py" reverting from last commit This reverts commit 76870dfebbcafee269b6e865677c6abaf14aec47.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import login, logout
 
 
 urlpatterns = [
-    url(r'^', views.startup, name = 'startup'),
+    url(r'^$', views.startup, name = 'startup'),
     url(r'^login/', login, {'template_name': 'accounts/login.html'}),
     url(r'^logout/', logout, {'template_name': 'accounts/logout.html'}),
     url(r'^signup/', views.signup, name='signup')

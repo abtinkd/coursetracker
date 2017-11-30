af432bd14dad5752b784ea5d6c914356eab4e92b
Alex <turneral1@grinnell.edu>
2017-11-30 14:20:32 -0800

    Tweaked settings variable organization. Streamlined integration testing, cutting down on superfluous operations. Expanded integration testing to work with the datepicker widgets.

--- history/forms.py
+++ history/forms.py
@@ -6,6 +6,7 @@ from datetimewidget.widgets import DateWidget
 class DateRangeForm(forms.Form):
     start_date = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3))
     end_date = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3))
+    # TODO dynamic min date https://stackoverflow.com/questions/40210999/how-to-disable-past-dates-in-bootstrap-datetimepicker-after-set-check-in-date
 
     def clean_end_date(self):
         if not self.cleaned_data['end_date'] >= self.cleaned_data['start_date']:
--- tracker/tests.py
+++ tracker/tests.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 class IntegrationTestCase(LiveServerTestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.get(self.live_server_url)
 
         # Mask long function names
         self.find_link, self.find_id, self.find_tag, = self.driver.find_element_by_link_text, \
@@ -14,10 +15,10 @@ class IntegrationTestCase(LiveServerTestCase):
                                                        self.driver.find_elements_by_tag_name
         self.find_name, self.find_class_name = self.driver.find_element_by_name, self.driver.find_elements_by_class_name
 
-        self.driver.get(self.live_server_url)
         self.sign_up('Marjan', 'CS541admin')
 
     def tearDown(self):
+        self.driver.refresh()
         try:
             self.driver.close()
         except (TypeError, ConnectionAbortedError, ConnectionResetError):
@@ -46,48 +47,40 @@ class IntegrationTestCase(LiveServerTestCase):
 
     def test_course_discretion(self):
         """Make sure that Users can only see their own Courses."""
+        # Go to the Courses page and add the "Marjan" Course
+        self.find_id("id_name").send_keys("Marjan")
+        self.find_id("id_hours").send_keys("10")
+        self.find_name("create").send_keys(Keys.ENTER)
+
+        # Switch to Abtin
         self.click('Log Out')
         self.sign_up(username='Abtin', password='CS541admin')
-        self.log_in(username='Abtin', password='CS541admin')
 
-        # Go to the Courses page and add the "Abtin" Course if it doesn't exist
-        self.click('Courses')
-        if not any([cell.text == "Abtin" for cell in self.find_class_name("name")]):
-            self.find_id("id_name").send_keys("Abtin")
-            self.find_id("id_hours").send_keys("10")
-            self.find_name("create").send_keys(Keys.ENTER)
-        self.click('Log Out')  # Abtin logs out
+        # Search for a Course with the name "Marjan"
+        self.assertFalse(any([cell.text == "Marjan" for cell in self.find_class_name("name")]))
 
-        self.log_in(username='Marjan', password='CS541admin')
-        self.click('Courses')  # go to Courses
+    def test_navigation(self):
+        """Ensure the User can navigate between all the major parts of the website."""
+        # Go to the Courses page and add a Course
+        self.find_id("id_name").send_keys("Test")
+        self.find_id("id_hours").send_keys("18")
+        self.find_name("create").send_keys(Keys.ENTER)
 
-        # Search for a Course with name "Abtin"
-        self.assertFalse(any([cell.text == "Abtin" for cell in self.find_class_name("name")]))
-
-    def test_course_timer_history(self):
-        """Ensure the User can add a Course, log study time, and view  History."""
-        self.log_in(username='Marjan', password='CS541admin')
-
-        # Go to the Courses page and add the "CS 1" Course if it doesn't exist
-        self.click('Courses')
-        if not any([cell.text == "CS 1" for cell in self.find_class_name("name")]):
-            self.find_id("id_name").send_keys("CS 1")
-            self.find_id("id_hours").send_keys("18")
-            self.find_name("create").send_keys(Keys.ENTER)
-
-        # Go to the Timer page, select the "CS 1" Course, and start the Timer
+        # Go to the Timer page, select the Course, and toggle the Timer
         self.click('Timer')
-        Select(self.find_id("id_course")).select_by_visible_text("CS 1")
+        Select(self.find_id("id_course")).select_by_visible_text("Test")
         self.find_id("playpause").send_keys(Keys.ENTER)
-
-        # Stop the Timer
         self.find_id("stopbutton").send_keys(Keys.ENTER)
 
         # Check that History presets work
-        for button_name in ('year', 'month', 'week', 'current'):  # check button presets
+        for button_name in ('year', 'month', 'week', 'current'):
             self.click('History')
             self.find_name(button_name).send_keys(Keys.ENTER)
 
-        # Check that custom History date ranges work - just make sure both calendars pop up
+        # Check that custom History date ranges work
         self.click('History')
-        self.assertEqual(len(self.driver.find_elements_by_xpath("//div[@class='datetimepicker-days']")), 2)
+        for datepicker, date_id in zip(self.driver.find_elements_by_xpath("//div[@class='datetimepicker-days']"),
+                                       ('id_start_date', 'id_end_date')):
+            self.find_id(date_id).click()
+            datepicker.find_elements_by_tag_name('td')[10].click()
+        self.find_name('custom').click()

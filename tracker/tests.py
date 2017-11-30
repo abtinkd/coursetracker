c184ca1eaf5c2987df51ebd176efb0f5fac9835c
Alex <turneral1@grinnell.edu>
2017-11-30 14:25:44 -0800

    Removed unnecessary integration test.

--- tracker/tests.py
+++ tracker/tests.py
@@ -15,7 +15,12 @@ class IntegrationTestCase(LiveServerTestCase):
                                                        self.driver.find_elements_by_tag_name
         self.find_name, self.find_class_name = self.driver.find_element_by_name, self.driver.find_elements_by_class_name
 
-        self.sign_up('Marjan', 'CS541admin')
+        # Sign up test user
+        self.click('Sign Up')
+        self.find_id("id_username").send_keys('admin')  # username
+        self.find_id("id_password1").send_keys('CS561Admin')  # password
+        self.find_id("id_password2").send_keys('CS561Admin')  # confirmation
+        self.find_tag("button")[1].send_keys(Keys.RETURN)
 
     def tearDown(self):
         self.driver.refresh()
@@ -27,37 +32,6 @@ class IntegrationTestCase(LiveServerTestCase):
     def click(self, link_string):
         """Shortcut for clicking on navigation elements."""
         self.find_link(link_string).send_keys(Keys.RETURN)
-
-    def log_in(self, username, password):
-        """Go to the login page and log in as the given user."""
-        self.click('Log In')
-        self.find_id("id_username").send_keys(username)
-        self.find_name("password").send_keys(password)
-        self.find_name("Submit").send_keys(Keys.RETURN)
-
-    def sign_up(self, username, password):
-        """Signs the user up and stays logged in."""
-        self.click('Sign Up')
-
-        # Enter information
-        self.find_id("id_username").send_keys(username)  # username
-        self.find_id("id_password1").send_keys(password)  # password
-        self.find_id("id_password2").send_keys(password)  # confirmation
-        self.find_tag("button")[1].send_keys(Keys.RETURN)
-
-    def test_course_discretion(self):
-        """Make sure that Users can only see their own Courses."""
-        # Go to the Courses page and add the "Marjan" Course
-        self.find_id("id_name").send_keys("Marjan")
-        self.find_id("id_hours").send_keys("10")
-        self.find_name("create").send_keys(Keys.ENTER)
-
-        # Switch to Abtin
-        self.click('Log Out')
-        self.sign_up(username='Abtin', password='CS541admin')
-
-        # Search for a Course with the name "Marjan"
-        self.assertFalse(any([cell.text == "Marjan" for cell in self.find_class_name("name")]))
 
     def test_navigation(self):
         """Ensure the User can navigate between all the major parts of the website."""

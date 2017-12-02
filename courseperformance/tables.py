acee86be9f7376a85249567e641c2fd78e648886
Alex <turneral1@grinnell.edu>
2017-12-01 18:59:52 -0800

    Fixed edge case for hours=0 in courses.forms

--- courseperformance/tables.py
+++ courseperformance/tables.py
@@ -1,10 +1,10 @@
 import django_tables2 as tables
+from timer.models import TimeInterval
 
 
 class TimeIntervalTable(tables.Table):
-    start_time = tables.Column()
-    length = tables.Column()
-
     class Meta:
+        model = TimeInterval
+
         attrs = {'class': 'paleblue'}
         exclude = ['id', 'course', ]

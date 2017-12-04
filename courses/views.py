bdb21695ccf6fe2156e353c0dd9d407d2f97194b
Alex <turneral1@grinnell.edu>
2017-12-04 08:56:17 -0800

    Courses now retains last active nav-tab when performing operations.

--- courses/views.py
+++ courses/views.py
@@ -6,28 +6,29 @@ from django.shortcuts import redirect, render
 
 
 @login_required
-def index(request):
-    """List the entered courses and ask the user for the name of the course they want to create."""
+def index(request):  # TODO integration test
+    """Allow viewing and manipulation of the user's Courses."""
     create_form, edit_form, delete_form = CreateCourseForm(user=request.user), EditCourseForm(user=request.user), \
                                           DeleteCourseForm(user=request.user)
+    tab = 'create'
 
     if request.method == 'POST':
         if 'create' in request.POST:
             create_form = CreateCourseForm(request.POST, user=request.user)
             if create_form.is_valid():
                 create_form.save(commit=True)
-                return redirect('/courses')
+                create_form = CreateCourseForm(user=request.user)
         elif 'edit' in request.POST:
-            edit_form = EditCourseForm(request.POST, user=request.user)
+            edit_form, tab = EditCourseForm(request.POST, user=request.user), 'edit'
             if edit_form.is_valid():
                 edit_form.save(commit=True)
-                return redirect('/courses')
+                edit_form = EditCourseForm(user=request.user)
         elif 'delete' in request.POST:
-            delete_form = DeleteCourseForm(request.POST, user=request.user)
+            delete_form, tab = DeleteCourseForm(request.POST, user=request.user), 'delete'
             if delete_form.is_valid():
                 delete_form.delete()
-                return redirect('/courses')
+                delete_form = DeleteCourseForm(user=request.user)
 
     return render(request, 'courses/index.html',
                   {'table': CourseTable(Course.objects.filter(user=request.user).order_by('name')),
-                   'create_form': create_form, 'edit_form': edit_form, 'delete_form': delete_form})
+                   'create_form': create_form, 'edit_form': edit_form, 'delete_form': delete_form, 'tab': tab})

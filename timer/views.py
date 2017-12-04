13549ba4763a7537a689c7c85b4960d1e9279621
Alex <turneral1@grinnell.edu>
2017-12-04 10:17:38 -0800

    Courses now retains last active nav-tab when performing operations. Expanded integration testing to check course nav-tabs. Simplified course view logic. Started simplifying courseperformance. Fixed issue with timer where start_time was 7 minutes too early.

--- timer/views.py
+++ timer/views.py
@@ -6,8 +6,9 @@ from django.utils import timezone
 
 
 @login_required
-def index(request):
+def index(request):  # TODO 0-error
     """Allow the user to create a TimeInterval."""
+    form = CourseSelectionForm(user=request.user)
     if request.method == 'POST':
         if 'start_time' in request.POST:  # get start_time from JS
             request.session.__setitem__('start_time', request.POST.get('start_time'))
@@ -18,11 +19,7 @@ def index(request):
                 course = form.cleaned_data['course']
                 start_time = timezone.datetime.strptime(request.session.__getitem__('start_time'), '%m-%d-%Y %H:%M:%S')
                 if not start_time.tzinfo:
-                    start_time = start_time.replace(tzinfo=timezone.get_current_timezone())
-                    start_time = start_time.astimezone(tz=timezone.utc)  # for use in database
+                    start_time = start_time.astimezone(timezone.get_current_timezone())
                 TimeInterval.objects.create(course=course, start_time=start_time)
                 return redirect('/courses')
-            else:
-                return render(request, 'timer/index.html', {'form': form})
-    else:
-        return render(request, 'timer/index.html', {'form': CourseSelectionForm(user=request.user)})
+    return render(request, 'timer/index.html', {'form': form})

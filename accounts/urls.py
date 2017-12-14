3990ee67eda4b17eaaabbfdb0c3429e36c31047d
Alex Turner <turneral1@grinnell.edu>
2017-12-14 11:42:38 -0600

    Implemented automated timezone detection. Removed unnecessary timezonefield-related files.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^login/', login, {'template_name': 'accounts/login.html'}),
     url(r'^logout/', logout, {'template_name': 'accounts/logout.html'}),
-    url(r'^settings/', views.settings, name='settings'),
     url(r'^signup/', views.signup, name='signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,37 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import SettingsForm, TimezoneUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
-def signup(request):  # TODO test TZ
+def signup(request):
     """Sign the user up and log them in."""
     if request.user.is_authenticated():  # they're already logged in
         return redirect('/courses')
-    form = TimezoneUserCreationForm()
+    form = UserCreationForm()
     if request.method == 'POST':
-        form = TimezoneUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password1'))
             auth_login(request, user)
-            return redirect('/courses')
+            return redirect('/')
     return render(request, 'accounts/signup.html', {'form': form})
 
 
 @login_required
 def logout(request):  # TODO redirect somewhere better
     return redirect('/accounts/logout.html')
-
-
-# TODO submit-less settings page
-@login_required
-def settings(request):
-    """Let the user configure account settings (currently only supports timezone switching)."""
-    if request.method == 'POST':
-        # TODO implement request.user = request.POST['timezone']
-        return redirect('/')
-    return render(request, 'accounts/settings.html', {'form': SettingsForm()})
 
 
 def welcome(request):
